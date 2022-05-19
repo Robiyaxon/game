d8b81ec9d711417f5600f89064a5b8a80b34096b
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-05-19 14:01:40 +0500

    Updates

---
+++ precache-manifest.22d6455ca1e72baf76898919e22bd53d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b342f02ee90979c10bbbf5c5a0297177",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "aad628e4661c70def1e7",
+    "url": "/game/static/css/main.d41169ba.chunk.css"
+  },
+  {
+    "revision": "5b4894e22cd56e6508ef",
+    "url": "/game/static/js/2.2fec3f20.chunk.js"
+  },
+  {
+    "revision": "1972f29574012851c652",
+    "url": "/game/static/js/3.1749d0fc.chunk.js"
+  },
+  {
+    "revision": "aad628e4661c70def1e7",
+    "url": "/game/static/js/main.a00abff3.chunk.js"
+  },
+  {
+    "revision": "a27e84e800078816eb86",
+    "url": "/game/static/js/runtime~main.270816a1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/game/static/media/logo.06e73328.svg"
+  }
+]);
